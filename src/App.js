cfeaf038f09642a21513b0363d3614ef2167ebf1
CADizon <caydizon.student@ua.edu.ph>
2024-03-22 23:39:19 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Resume from './resume'; // Assuming Resume component is in the same directory
+
+function App() {
+  return (
+    <div className="App">
+      <Resume />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/resume.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import './styles.css';
+
+const Resume = () => {
+    return (
+        <div style={{ backgroundColor: 'gainsboro' }}>
+            <table style={{ width: '600px', fontSize: '12px', border: '2px solid transparent', borderCollapse: 'collapse', fontFamily: 'Arial', backgroundColor: 'white', tableLayout: 'fixed' }}>
+                <tbody>
+                    <tr>
+                        <td colSpan="4" rowSpan="8" align="center" style={{ maxWidth: '100%', maxHeight: '100%', padding: '0' }}>
+                            <img src="https://i.imgur.com/0rP5tnj.jpeg" style={{ width: '100%', height: '100%', display: 'block', margin: 'auto' }} alt="Profile" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan="4"></td>
+                    </tr>
+                    <tr style={{ fontSize: '35px' }}>
+                        <th colSpan="4" rowSpan="2">Carl Andrew Y. Dizon</th>
+                    </tr>
+                    <tr></tr>
+                    <tr>
+                        <td colSpan="4" style={{ fontSize: '30px' }}>College Graduate</td>
+                    </tr>
+                    <tr style={{ fontSize: '20px', color: 'crimson' }}>
+                        <th colSpan="4" rowSpan="2">MAIN OBJECTIVE</th>
+                    </tr>
+                    <tr></tr>
+                    <tr>
+                        <td colSpan="4" rowSpan="5" style={{ fontSize: '15px' }}>- I have four years of experience as a game developer in the entertainment industry. I am looking to use my problem-solving skills and gaming experience to effectively contribute to the company while learning entrepreneurial opportunities within an innovative company that values creativity and pushes its employees to think creatively.</td>
+                    </tr>
+                    <tr>
+                        <th colSpan="4" rowSpan="2" style={{ fontSize: '20px', backgroundColor: 'crimson', color: 'white' }}>Contact</th>
+                    </tr>
+                    <tr></tr>
+                    <tr>
+                        <td colSpan="4" style={{ backgroundColor: 'crimson', color: 'white', fontSize: '15px' }}>+63 947 225 6228</td>
+                    </tr>
+                    <tr>
+                        <td colSpan="4" style={{ backgroundColor: 'crimson', color: 'white', fontSize: '15px' }}>www.carlandrewdizon.com</td>
+                    </tr>
+                    <tr>
+                        <td colSpan="4" style={{ backgroundColor: 'crimson', color: 'white', fontSize: '15px' }}>caydizon.student@ua.edu.ph</td>
+                        <th colSpan="4" rowSpan="2" style={{ fontSize: '20px', color: 'crimson' }}>WORK EXPERIENCE</th>
+                    </tr>
+                    <tr>
+                        <td colSpan="4" style={{ backgroundColor: 'crimson', color: 'white', fontSize: '15px' }}>Brgy. Lourdes, City of San Fernando Pampanga C-2000</td>
+                    </tr>
+                    <tr style={{ fontSize: '20px' }}>
+                        <th colSpan="4" rowSpan="2" style={{ fontSize: '20px', backgroundColor: 'crimson', color: 'white' }}>INTEREST</th>
+                        <td colSpan="4" style={{ fontSize: '15px' }}>Game tester and Character designer</td>
+                    </tr>
+                    <tr>
+                        <td colSpan="3" style={{ fontSize: '14px' }}><strong>Nintendo | Minami-ku, Kyoto, Japan | April 2028 to May 2032</strong></td>
+                    </tr>
+                    <tr>
+                        <td colSpan="4" style={{ backgroundColor: 'crimson', color: 'white', fontSize: '15px' }}>- Gaming</td>
+                        <td colSpan="4" rowSpan="2" style={{ fontSize: '15px' }}>- Worked together with designers and artists to integrate audio, visual effects, and animations into the game experience while maintaining a unified creative vision.</td>
+                    </tr>
+                    <tr>
+                        <td colSpan="4" style={{ backgroundColor: 'crimson', color: 'white', fontSize: '15px' }}>- Greek mythology</td>
+                    </tr>
+                    <tr>
+                        <td colSpan="4" style={{ backgroundColor: 'crimson', color: 'white', fontSize: '15px' }}>- Norse mythology</td>
+                        <td colSpan="4" rowSpan="2" style={{ fontSize: '15px' }}>- Optimized memory consumption and game performance to guarantee a smooth gaming experience on lower-end hardware and mobile devices.</td>
+                    </tr>
+                    <tr>
+                        <td colSpan="4" style={{ backgroundColor: 'crimson', color: 'white', fontSize: '15px' }}>- Egyptian mythology</td>
+                    </tr>
+                    <tr>
+                        <td colSpan="4" style={{ backgroundColor: 'crimson', color: 'white', fontSize: '15px' }}>- Watching anime</td>
+                    </tr>
+                    <tr>
+                        <th style={{ fontSize: '20px', backgroundColor: 'crimson', color: 'white' }} colSpan="4" rowSpan="2">ACHIEVEMENTS</th>
+                        <td colSpan="4"></td>
+                    </tr>
+                    <tr></tr>
+                    <tr>
+                        <td colSpan="4" style={{ backgroundColor: 'crimson', color: 'white', fontSize: '15px' }}>- Graduate as a Magna Cumlaude in College (2027)</td>
+                        <th colSpan="4" rowSpan="2" style={{ fontSize: '20px', color: 'crimson' }}>ACADEMIC BACKGROUND</th>
+                    </tr>
+                    <tr>
+                        <td colSpan="4" style={{ backgroundColor: 'crimson', color: 'white', fontSize: '15px' }}>- With high honor in senior highschool (2021)</td>
+                    </tr>
+                    <tr>
+                        <td colSpan="4" style={{ backgroundColor: 'crimson', color: 'white', fontSize: '15px' }}>- 2nd place, taekwondo competition (2021)</td>
+                        <td colSpan="4" style={{ fontSize: '15px' }}><strong>BACHELOR OF SCIENCE IN COMPUTER ENGINEERING</strong></td>
+                    </tr>
+                    <tr>
+                        <td colSpan="4" style={{ backgroundColor: 'crimson', color: 'white', fontSize: '15px' }}>- 1st place, Deadfrontier2 contest (2020)</td>
+                        <td colSpan="4" style={{ fontSize: '15px' }}>University of the Assumption | 2022-2027</td>
+                    </tr>
+                    <tr>
+                        <td colSpan="4" style={{ backgroundColor: 'crimson', color: 'white', fontSize: '15px' }}>- 2nd place, spelling bee competition (2016)</td>
+                        <td colSpan="4" style={{ fontSize: '15px' }}><strong>HIGH SCHOOL DEGREE</strong></td>
+                    </tr>
+                    <td colSpan="4" style={{ backgroundColor: 'crimson', color: 'white', fontSize: '15px' }}></td>
+                        <td colSpan="4" style={{ fontSize: '15px' }}>Pampanga High School | 2016-2022</td>
+                    <tr>
+                        <td colSpan="4" style={{ backgroundColor: 'crimson', color: 'white', fontSize: '15px' }}></td>
+                        <td colSpan="4"></td>
+                    </tr>
+                    <tr>
+                        <td colSpan="4" style={{ backgroundColor: 'crimson', color: 'white', fontSize: '15px' }}></td>
+                        <td colSpan="4"></td>
+                    </tr>
+                    <tr>
+                        <td colSpan="4" style={{ backgroundColor: 'crimson', color: 'white', fontSize: '15px' }}></td>
+                        <td colSpan="4"></td>
+                    </tr>
+                    <tr>
+                        <td colSpan="4" style={{ backgroundColor: 'crimson', color: 'white', fontSize: '15px' }}></td>
+                        <td colSpan="4"></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Resume;
